3cc7bbcb43e3da22e372a87f715cafdb5aac4a12
Andy Chukavin <megadestroyer007@gmail.com>
2021-12-23 17:32:16 +0400

    Added empty app

---
+++ BlockchainSdkExample/BlockchainSdkExample/BlockchainSdkExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockchainSdkExampleApp.swift
+//  BlockchainSdkExample
+//
+//  Created by Andy on 23.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BlockchainSdkExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
